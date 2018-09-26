3fbb361470e23697df8590989074476b4b1809fb
charsythe <booktearex@gmail.com>
2018-09-25 23:04:08 -0400

    Added ignore for .pyc, added ship to project.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # Initialize pygame, settings, and screen object
@@ -15,6 +16,9 @@ def run_game():
     # Set the background color.
     bg_color = (230, 230, 230)
 
+    # Make a ship.
+    ship = Ship(screen)
+
     # Start the main loop for the game.
     while True:
 
@@ -25,6 +29,7 @@ def run_game():
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
