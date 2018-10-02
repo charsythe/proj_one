f1a85147f6912fcb0dfa373f2e4f55c3ba171738
charsythe <booktearex@gmail.com>
2018-10-02 12:01:10 -0400

    Added highscore per session.

--- game_stats.py
+++ game_stats.py
@@ -9,6 +9,9 @@ class GameStats():
         # Start in inactive state.
         self.game_active = False
 
+        # High score should not be reset.
+        self.high_score = 0
+
     def reset_stats(self):
         """Init stats that change during gameplay."""
         self.ships_left = self.ai_settings.ship_limit
--- scoreboard.py
+++ scoreboard.py
@@ -16,6 +16,7 @@ class Scoreboard():
 
         # Prepare initial score image
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """Turn score into rendered image."""
@@ -29,7 +30,20 @@ class Scoreboard():
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """Turn high score into rendered image."""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True,
+            self.text_color, self.ai_settings.bg_color)
+
+        # Center high score at top of screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
 
     def show_score(self):
         """Draw score to screen."""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
