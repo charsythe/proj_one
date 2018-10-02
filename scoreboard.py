a6092a6de23110143bc593cacf66bdb73312bc5c
charsythe <booktearex@gmail.com>
2018-10-02 11:43:23 -0400

    Added commas to make it look fancy and neat.

--- scoreboard.py
+++ scoreboard.py
@@ -19,7 +19,8 @@ class Scoreboard():
 
     def prep_score(self):
         """Turn score into rendered image."""
-        score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, self.text_color,
             self.ai_settings.bg_color)
 
@@ -27,6 +28,7 @@ class Scoreboard():
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
+
 
     def show_score(self):
         """Draw score to screen."""
